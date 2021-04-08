075fbde82db9321abb169fa5d0dfb47d0b9dd26e
matthewwatthew <58442210+matthewwatthew@users.noreply.github.com>
2021-04-08 18:02:02 -0400

    Add files via upload

---
+++ Clustering/Point.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class Point {
+
+    public double[] cordinates;
+
+
+    /**
+     * Constructor
+     * @param cordinates vector of cordinates example [x, y, z]
+     */
+    public Point(double[] cordinates){
+        this.cordinates = cordinates;
+    }
+
+
+    /**
+     * Returns a string respresentation fo the points
+     * @return
+     */
+    public String toString() {
+        return Arrays.toString(this.cordinates);
+
+    }
+
+
+    /**
+     * Return the euclidean distance to a point.
+     * @param point
+     * @return
+     */
+    public double distance(Point point) {
+        //TODO: Implement this method
+        double distance = 0;
+        for(int i = 0; i < this.cordinates.length; i++){
+            distance += Math.pow((this.cordinates[i] - point.cordinates[i]),2);
+        }
+        return Math.sqrt(distance);
+
+
+    }
+    public static void main(String[] arg){
+        double[] papa = {0,1};
+        double[] mama = {1,0};
+        Point p = new Point(papa);
+        Point d = new Point(mama);
+        System.out.println(p.distance(d));
+
+    }
+}
